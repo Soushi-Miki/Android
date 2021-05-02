4d6e6ddaa8d34b37f71a9b62fb4b89139e120944
Soushi-Miki <s2081145@s.konan-u.ac.jp>
2021-05-02 13:28:03 +0900

    サウンドアプリ2の完成

--- app/src/main/java/app/miki/mikiso/sound/MainActivity.kt
+++ app/src/main/java/app/miki/mikiso/sound/MainActivity.kt
@@ -14,20 +14,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //ドラムのサウンドファイルを読み込んで、プレイヤーをつくる
         val drumSound = MediaPlayer.create(this, R.raw.drum_sound)
 
-
+        //ドラムがタッチされた時に呼ばれる
         drumImage.setOnTouchListener { view, event ->
 
-
+            //タッチが始まった時に
             if (event.action == MotionEvent.ACTION_DOWN) {
 
+                //ドラムが鳴っている画像に切り替える
                 drumImage.setImageResource(R.drawable.drum_playing_image)
+                //ドラムの音を巻き戻す
                 drumSound.seekTo(0)
+                //ドラムの音を再生する
                 drumSound.start()
             }
+            //タッチが終わるときに
             else if (event.action==MotionEvent.ACTION_UP){
-
+                //ドラムが鳴っていない画像に切り替える
                 drumImage.setImageResource(R.drawable.drum_image)
             }
 
